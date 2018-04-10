893bad97273ce7b23d21f517ed091e3091bf1589
lonmoon <noomnol@163.com>
2018-04-11 00:44:52 +0800

    Set up context.

--- GLKitDemo/ViewController.swift
+++ GLKitDemo/ViewController.swift
@@ -6,20 +6,30 @@
 //  Copyright © 2018年 Lonmoon. All rights reserved.
 //
 
-import UIKit
+import GLKit
 
-class ViewController: UIViewController {
-
+class ViewController: GLKViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        setupContext()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func setupContext() {
+        let glkView = view as! GLKView
+        glkView.context = EAGLContext(api: .openGLES3)!
+    }
+    
+    override func glkView(_ view: GLKView, drawIn rect: CGRect) {
+        glClearColor(0.6, 0.8, 1.0, 1.0)
+        glClear(GLbitfield(GL_COLOR_BUFFER_BIT))
+    }
 }
 
